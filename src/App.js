a0da2515e4c8581c1d5ed2bf412006bea90830ce
yetemegn-telaye <lemma19yet@gmail.com>
2023-06-21 13:36:51 +0300

    Add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       Hello World
+      <Button as="a" variant="success">Hello Bootstrap</Button>
     </div>
   );
 }
