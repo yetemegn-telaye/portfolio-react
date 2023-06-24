b459e46b6b087d405ef362f5f961835d5269b6b5
yetemegn-telaye <lemma19yet@gmail.com>
2023-06-24 23:57:53 +0300

    Create basic structure for projects

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const WorkSection = () => (
-  <div>
-    <h1>Work Section</h1>
-  </div>
+  <Container className="py-4 my-4">
+    <div className="works-title d-flex flex-lg-column flex-md-column">
+      <h2 className="mb-3">My Recent Works</h2>
+      <hr className="w-75 mt-4" />
+    </div>
+    <div className="projects">
+      <Row className="justify-content-center align-items-center">
+        <Col sm={12} md={6}>Project 1</Col>
+        <Col sm={12} md={6}>Project 2</Col>
+      </Row>
+      <Row className="justify-content-center align-items-center">
+        <Col sm={12} md={6}>Project 3</Col>
+        <Col sm={12} md={6}>Project 4</Col>
+      </Row>
+    </div>
+  </Container>
 );
 export default WorkSection;
